1c33867467fa795499eeea5707d989bf99631321
edreja <endr224al@gmail.com>
2026-06-05 00:53:38 +0200

    fix: make staff role validator case-insensitive

--- src/backend/DashTab.Application/Validators/CreateStaffRequestValidator.cs
+++ src/backend/DashTab.Application/Validators/CreateStaffRequestValidator.cs
@@ -10,6 +10,6 @@ public class CreateStaffRequestValidator : AbstractValidator<CreateStaffRequest>
         RuleFor(x => x.FullName).NotEmpty().MaximumLength(200);
         RuleFor(x => x.Email).NotEmpty().EmailAddress();
         RuleFor(x => x.Password).NotEmpty().MinimumLength(8);
-        RuleFor(x => x.Role).NotEmpty().Must(r => new[] { "Owner", "Manager", "Waiter", "Kitchen" }.Contains(r));
+        RuleFor(x => x.Role).NotEmpty().Must(r => new[] { "Owner", "Manager", "Waiter", "Kitchen" }.Contains(r, StringComparer.OrdinalIgnoreCase));
     }
 }
